d8f02ee8f54748562bb70e7da63451a82cc3a828
pykko <pykko@xilim.fr>
2020-12-04 11:20:30 +0100

    Scripts List

---
+++ testerAffRestosNuits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+
+def affRestosNuits( lstNbRestos ) :
+	
+	for i in range( 0 , len( lstNbRestos ) ) :
+		numNuit = i + 1
+		print( 'Nuit du' , numNuit , ':' , lstNbRestos[ i ] )
+
+
+if __name__ == "__main__" :
+
+	ouvertsParisNuit = [ 
+			103 , 89 , 45 , 82 , 35 , 101 , 129 , 23 , 32 , 0 , 
+			0 ,  86 , 27 , 102 , 203 , 109 , 27 , 92 , 102 , 92 ,
+			72 , 90 , 0 , 25 , 175 , 9 , 28 , 33 , 90 , 83
+		]
+	
+	affRestosNuits( ouvertsParisNuit )
---
+++ testerGetNbMaxRestosOuverts.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+def getNbMaxRestosOuverts( lstNbRestos ) :
+	
+	nbMax = lstNbRestos[ 0 ]
+	
+	for n in lstNbRestos :
+		if n > nbMax :
+			nbMax = n
+			
+	return nbMax
+	
+
+
+if __name__ == "__main__" :
+
+	ouvertsParisNuit = [ 
+			103 , 89 , 45 , 82 , 35 , 101 , 159 , 23 , 32 , 0 , 
+			0 ,  86 , 27 , 102 , 203 , 109 , 27 , 92 , 102 , 92 ,
+			72 , 90 , 0 , 25 , 175 , 9 , 28 , 33 , 90 , 83
+		]
+	
+	nbMax = getNbMaxRestosOuverts( ouvertsParisNuit )
+	
+	print( "Ce mois, le nombre maximum de restaurants ouverts en une nuit est de" , nbMax , "." )
