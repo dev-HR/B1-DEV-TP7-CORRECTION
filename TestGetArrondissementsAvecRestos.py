7b0394831ef23d328100a7fcced05cdb13e51506
pykko <pykko@xilim.fr>
2020-12-04 13:52:08 +0100

    Scripts Méthodes

--- TestGetArrondissementsAvecRestos.py
+++ TestGetArrondissementsAvecRestos.py
@@ -3,59 +3,62 @@
 
 
 def getNom( unResto ) :
-    ( nom , arrondissement ) = unResto.split( ':' )
-    return nom
+	( nom , arrondissement ) = unResto.split( ':' )
+	return nom
 
 def getArrondissement( unResto ) :
-    ( nom , arrondissement ) = unResto.split( ':' )
-    return int( arrondissement )
+	( nom , arrondissement ) = unResto.split( ':' )
+	return int( arrondissement )
 
 
 
 def getArrondissementsAvecRestos( ) :
-    global restos
-    lesArr = []
+	global restos
+	lesArr = []
 
-    # VOTRE CODE ICI
+	for unResto in restos :
+		arrondissement = getArrondissement( unResto )
+		if arrondissement not in lesArr :
+			lesArr.append( arrondissement )
 
-    return lesArr
+	return lesArr
 
 
 
 if __name__ == '__main__' :
 
-    restos = [
-        'Turkish snack:11' ,
-        'Tour Food:7' ,
-        'Restaurant Gül:11' ,
-        'Kebab House:11' ,
-        'Apollon:7' ,
-        'Zarma:9' ,
-        'Restaurant Alim:11' ,
-        'Art Kebab:7' ,
-        'Coriandre:7' ,
-        'Pacha kebab:9' ,
-        'Paristanbul:7' ,
-        'Restaurant Efes:11' ,
-        'Firat:11' ,
-        'Paristanbul:2' ,
-        'Restaurant Dilan:2' ,
-        'Pacha kebab:11' ,
-        'Mac Doner:11' ,
-        'Le Giros:7' ,
-        'Chez Yamina:9' ,
-        'Pita Kebab:1' ,
-        'Al Boustan:1' ,
-        'Le Bosphore:1' ,
-        'Planet Istanbul:11' ,
-        'Istanbul kebab:11' ,
-        'Diyar:7' ,
-        'Nabab Kebab:1' ,
-        'Grillé:2' ,
-        'Chilane:7'
-    ]
+	restos = [
+		'Turkish snack:11' ,
+		'Tour Food:7' ,
+		'Restaurant Gül:11' ,
+		'Kebab House:11' ,
+		'Apollon:7' ,
+		'Zarma:9' ,
+		'Restaurant Alim:11' ,
+		'Art Kebab:7' ,
+		'Coriandre:7' ,
+		'Pacha kebab:9' ,
+		'Paristanbul:7' ,
+		'Restaurant Efes:11' ,
+		'Firat:11' ,
+		'Paristanbul:2' ,
+		'Restaurant Dilan:2' ,
+		'Pacha kebab:11' ,
+		'Mac Doner:11' ,
+		'Le Giros:7' ,
+		'Chez Yamina:9' ,
+		'Pita Kebab:1' ,
+		'Al Boustan:1' ,
+		'Le Bosphore:1' ,
+		'Planet Istanbul:11' ,
+		'Istanbul kebab:11' ,
+		'Diyar:7' ,
+		'Nabab Kebab:1' ,
+		'Grillé:2' ,
+		'Chilane:7'
+	]
 
-    lesArrondissements = getArrondissementsAvecRestos()
+	lesArrondissements = getArrondissementsAvecRestos()
 
-    for unArrondissement in lesArrondissements :
-        print( unArrondissement )
+	for unArrondissement in lesArrondissements :
+		print( unArrondissement )
--- TestSuppRestosDansArrondissement.py
+++ TestSuppRestosDansArrondissement.py
@@ -3,56 +3,63 @@
 
 
 def getNom( unResto ) :
-    ( nom , arrondissement ) = unResto.split( ':' )
-    return nom
+	( nom , arrondissement ) = unResto.split( ':' )
+	return nom
 
 def getArrondissement( unResto ) :
-    ( nom , arrondissement ) = unResto.split( ':' )
-    return int( arrondissement )
+	( nom , arrondissement ) = unResto.split( ':' )
+	return int( arrondissement )
 
 def suppRestosDansArrondissement( numArrondissement ) :
-    global restos
+	global restos
+	
+	cpt = 0
 
-    # VOTRE CODE ICI
+	for i in range( len( restos ) - 1 , -1 , -1 ) :
+		if getArrondissement( restos[ i ] ) == numArrondissement :
+			restos.pop( i )
+			cpt = cpt + 1
+			
+	return cpt
 
 
 
 if __name__ == '__main__' :
 
-    restos = [
-        'Turkish snack:11' ,
-        'Tour Food:7' ,
-        'Restaurant Gül:11' ,
-        'Kebab House:11' ,
-        'Apollon:7' ,
-        'Zarma:9' ,
-        'Restaurant Alim:11' ,
-        'Art Kebab:7' ,
-        'Coriandre:7' ,
-        'Pacha kebab:9' ,
-        'Paristanbul:7' ,
-        'Restaurant Efes:11' ,
-        'Firat:11' ,
-        'Paristanbul:2' ,
-        'Restaurant Dilan:2' ,
-        'Pacha kebab:11' ,
-        'Mac Doner:11' ,
-        'Le Giros:7' ,
-        'Chez Yamina:9' ,
-        'Pita Kebab:1' ,
-        'Al Boustan:1' ,
-        'Le Bosphore:1' ,
-        'Planet Istanbul:11' ,
-        'Istanbul kebab:11' ,
-        'Diyar:7' ,
-        'Nabab Kebab:1' ,
-        'Grillé:2' ,
-        'Chilane:7'
-    ]
+	restos = [
+		'Turkish snack:11' ,
+		'Tour Food:7' ,
+		'Restaurant Gül:11' ,
+		'Kebab House:11' ,
+		'Apollon:7' ,
+		'Zarma:9' ,
+		'Restaurant Alim:11' ,
+		'Art Kebab:7' ,
+		'Coriandre:7' ,
+		'Pacha kebab:9' ,
+		'Paristanbul:7' ,
+		'Restaurant Efes:11' ,
+		'Firat:11' ,
+		'Paristanbul:2' ,
+		'Restaurant Dilan:2' ,
+		'Pacha kebab:11' ,
+		'Mac Doner:11' ,
+		'Le Giros:7' ,
+		'Chez Yamina:9' ,
+		'Pita Kebab:1' ,
+		'Al Boustan:1' ,
+		'Le Bosphore:1' ,
+		'Planet Istanbul:11' ,
+		'Istanbul kebab:11' ,
+		'Diyar:7' ,
+		'Nabab Kebab:1' ,
+		'Grillé:2' ,
+		'Chilane:7'
+	]
 
-    nbSupp = suppRestosDansArrondissement( 11 )
+	nbSupp = suppRestosDansArrondissement( 11 )
 
-    print( 'Nombre de restaurants supprimés :' , nbSupp )
+	print( 'Nombre de restaurants supprimés :' , nbSupp )
 
-    for unResto in restos :
-        print( unResto )
+	for unResto in restos :
+		print( unResto )
